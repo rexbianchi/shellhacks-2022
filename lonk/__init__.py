cd9608600ee31e6edb8e6d650af9c19a9994def9
Rex Bianchi <rexbianchi@protonmail.com>
2022-09-10 02:36:42 -0400

    x`

--- lonk/__init__.py
+++ lonk/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_awscognito import AWSCognitoAuthentication
+from flask_jwt_extended import JWTManager
+
+
+# Globally accessable libraries
+aws_auth = AWSCognitoAuthentication()
+jwt_manager = JWTManager()
+
 
 def create_app():
     app = Flask(__name__)
+
+    #
+    aws_auth.init_app(app)
+    jwt_manager.init_app(app)
+
     app.config['SECRET_KEY'] = 'lonkes-awakening'
 
     from .views import views
---
+++ lonk/cognito.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, url_for, request, redirect, make_response, render_template
+
+from flask_jwt_extended import set_access_cookies, verify_jwt_in_request, get_jwt_identity
+
+from . import aws_auth, jwt_manager
+
+# 30 min cookie len
+MAX_COOKIE_AGE = 30 * 60
+
+bp = Blueprint('cognito', __name__)
+
+
+@bp.route('/log_in')
+def log_in():
+    return redirect(aws_auth.get_sign_in_url())
+
+
+@bp.route('/logged_in')
+def logged_in():
+    access_token = aws_auth.get_access_token(request.args)
+    response = make_response(redirect(url_for("protected")))
+    set_access_cookies(response, access_token, max_age=MAX_COOKIE_AGE)
+    return response
+
+
+@bp.route('/secret')
+@verify_jwt_in_request(optional=True)
+def protected():
+    if get_jwt_identity():
+        return render_template("secret.html")
+    else:
+        return redirect(aws_auth.get_sign_in_url())
+
+
