13acc0275d260b17f0f4a28c8849023bbc716a18
Mason Williams <mason@Masons-MacBook-Air.local>
2022-09-10 07:13:27 -0400

    little chnages

--- lonk/front-end-test.py
+++ lonk/front-end-test.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("register.html")
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
