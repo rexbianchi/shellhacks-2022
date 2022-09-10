cc062382d82cc8286ce049809cbd99f06fd20106
Mason Williams <mason@Masons-MacBook-Air.local>
2022-09-10 07:11:10 -0400

    Added login Jinja template and front end test script

---
+++ lonk/front-end-test.py
@@ -0,0 +1,12 @@
+# app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("register.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
